56a38b7ef1baa995bb2ad224388933aeb90568ce
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2026-09-01 22:43:50 -0300

    feat: automatiza fluxo E2E de cadastro e login

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,28 +1,31 @@
-describe('Testes End To End do fluxo de cadastro e login', () => {
+import { faker } from '@faker-js/faker';
 
-    /* 
-    Testes End To End ou Testes de ponta a ponta, ligam uma série de funcionalidades de um sistema,
-    simulando o comportamento do usuário final. Esses testes verificam se diferentes partes do sistema
-    funcionam corretamente quando integradas, garantindo que o fluxo completo de uma funcionalidade
-    funcione como esperado.
-    Aqui iremos criar um teste end to end que cobre o fluxo de cadastro e login de um usuário em um sistema web.
-    Em apenas um teste, ou seja, em um único "it", iremos:
-    1. Acessar a página de cadastro.
-    2. Preencher o formulário de cadastro com dados válidos.
-    3. Submeter o formulário e verificar se o cadastro foi bem-sucedido.
-    4. Acessar a página de login.
-    5. Preencher o formulário de login com as credenciais do usuário recém-cadastrado.
-    6. Submeter o formulário de login e verificar se o login foi bem-sucedido.
+describe("Testes End To End do fluxo de cadastro e login", () => {
 
-    Use as boas práticas aprendidas até agora para estruturar o teste.
-    */
+  it("Deve fazer o cadastro e validar o login com o usuário cadastrado", () => {
 
-    beforeEach(() => {
-        // Configurações iniciais, se necessário
-    });
+    const email = faker.internet.email();
+    const senha = "qa1234";
 
+    // Cadastro
+    cy.visit("http://localhost:3000/register.html");
 
-    it('Deve fazer o cadastro e validar o login com o usuário cadastrado', () => {
-        // Criar todo o fluxo aqui dentro deste único "it"
-    });
+    cy.get("#name").type("Edson QA");
+    cy.get("#email").type(email);
+    cy.get("#phone").type("2456785");
+    cy.get("#password").type(senha);
+    cy.get("#confirm-password").type(senha);
+    cy.get("#terms-agreement").click();
+    cy.get("#register-btn").click();
+
+    // Validação do cadastro
+    cy.contains("Conta criada com sucesso! Fazendo login...")
+      .should("be.visible");
+
+    // Login
+    cy.visit("http://localhost:3000/login.html");
+
+    cy.login(email, senha);
+  });
+
 });
