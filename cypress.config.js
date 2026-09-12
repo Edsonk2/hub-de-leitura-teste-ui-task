c99a773dfc8626e3f0b4cc79a9b0c18af769aee7
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2026-09-11 22:28:55 -0300

    Configura GitHub Actions e relatório de testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
     baseUrl: "http://localhost:3000/",
     video: true
-  },
+  }
 });
